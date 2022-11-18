32a8b86cf93b916f4b113c09f3600a49785d5511
CSI1-Joreuez <CSI1-Joreuez@colegioaltair.net>
2022-11-18 12:01:53 +0100

    No terminado por culpa de no detalles

---
+++ DAL/Modelo/Empleado.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace DAL.Modelo;
+
+public partial class Empleado
+{
+    public long Id { get; set; }
+
+    public string? Nombre { get; set; }
+
+    public string? Apellidos { get; set; }
+
+    public long AreaId { get; set; }
+
+    public virtual Area Area { get; set; } = null!;
+}
---
+++ MVC completo-4/Controllers/Empleados.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using DAL.Modelo;
+namespace MVC_completo_4.Controllers
+{
+    public class Empleados : Controller
+    {
+        public DAL.Modelo.MvcContext db = new MvcContext();
+        //GET EMPLEADOS
+        public IActionResult Index()
+        {
+            var empleados = db.Empleados.ToList();
+            HttpContext.Session.GetString(empleados.ToString());
+            return View(empleados);
+        }
+        [HttpPost]
+        public ActionResult Index(int area_id)
+        {
+            Empleado empleado = db.Empleados.Where(e => e.AreaId == area_id).FirstOrDefault();
+            if (empleado != null)
+            {
+                ViewBag.Area = "Empleado:" + empleado.Nombre + " - Area:" + empleado.Area.Area1;
+            }
+            return View(HttpContext.Session.GetString(empleado.ToString()));
+        }
+    }
+}
